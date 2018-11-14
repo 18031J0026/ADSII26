175f8d40869e188ea3334d5b5aee2c9554bddbce
GannavaramPravalika <pravalikagannavaram@gmail.com>
2018-11-14 09:50:56 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

---
+++ m2/2.1 CC_Percolation/CC.java
@@ -0,0 +1,67 @@
+public class CC {
+boolean[] marked;
+int[] id;
+int[] size;
+int count;
+public CC(Graph g)
+{
+	marked= new boolean[g.V()+1];
+	id=new int[g.V()+1];
+	size=new int[g.V()+1];
+	for(int v=1;v<=g.V();v++)
+	{
+		if(!marked[v])
+		{
+			dfs(g,v);
+			count++;
+		}
+	}
+}
+void dfs(Graph g,int v)
+{
+	marked[v]=true;
+	id[v]=count;
+	for(int w:g.adj(v))
+	{
+		if(!marked[w])
+		{
+			dfs(g,w);
+		}
+	}
+}
+public int id(int v) {
+    validatevertex(v);
+    return id[v];
+}
+public int count() {
+    return count;
+}
+public int size(int v) {
+    validatevertex(v);
+    return size[id[v]];
+}
+
+boolean connected(int v,int w)
+{
+	
+	validatevertex(v);
+	validatevertex(w);
+	return id[v]==id[w];
+}
+void validatevertex(int v)
+{
+	int V=marked.length;
+	if(v<0||v>=V)
+		throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+}
+void display()
+{
+	
+	if(count==1)
+	{
+		System.out.println("true");
+	}
+	else
+		System.out.println("false");
+}
+}
